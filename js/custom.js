1e1e28d9096e1288d06736093055538e97887b1a
lauramgrisalesgarcia <41131779+lauramgrisalesgarcia@users.noreply.github.com>
2020-10-23 21:35:03 -0500

    Template update

--- js/custom.js
+++ js/custom.js
@@ -155,7 +155,7 @@
 			marker = new google.maps.Marker({ 
 			position: new google.maps.LatLng(locations[i][1], locations[i][2]), 
 			map: map ,
-			icon: 'images/marker.png'
+			icon: 'images/location.png'
 			});
 	
 	
